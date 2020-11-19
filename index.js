d25332cea12aeed1565fbbb7d1b9dda9c3e2d53e
Manish <54002696+manishgour70@users.noreply.github.com>
2020-11-19 09:35:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.use(express.json())
 
 var schema={sensor_id:1, creation_date:1, height:1}
 
+var schema_fall={ __v:1}
+
+
 app.get('/smartcontainer/currentDate',function (req, res) {
     var date1=new Date()
     var date2=new Date()
@@ -30,8 +33,8 @@ app.get('/smartcontainer/currentDate',function (req, res) {
     
     date3=new Date(date1.setTime(date1.getTime()+date1.getTimezoneOffset()*60*1000));
     date4=new Date(date2.setTime(date2.getTime()+date2.getTimezoneOffset()*60*1000));
-//     console.log(date3)
-//     console.log(date4)
+    console.log(date3)
+    console.log(date4)
     User.find({"creation_date": {$gte:date1.toISOString(),$lte:date2.toISOString()}},schema).then((user) => {
         // console.log(user)
     res.status(201).send(user)
@@ -39,7 +42,6 @@ app.get('/smartcontainer/currentDate',function (req, res) {
         res.status(400).send(e)
     })
 })
-
   
 app.get('/smartcontainer/range', (req, res) => {
 
@@ -74,7 +76,50 @@ app.post("/smartcontainer/insert", (req, res) => {
 
 })
 
+app.get('/smartcontainer/currentDate_fall',function (req, res) {
+    var date1=new Date()
+    var date2=new Date()
     
+    if(Object.keys(req.query).length === 0)
+    {
+        date2.setDate(date2.getDate()+1)
+    }
+    else{
+        date2=new Date(req.query.date1)
+        date1=new Date(req.query.date1)
+        date2.setDate(date1.getDate()+1)
+    }
+    
+    date3=new Date(date1.setTime(date1.getTime()+date1.getTimezoneOffset()*60*1000));
+    date4=new Date(date2.setTime(date2.getTime()+date2.getTimezoneOffset()*60*1000));
+    console.log(date3)
+    console.log(date4)
+    User.find({"creation_date": {$gte:date1.toISOString(),$lte:date2.toISOString()}},schema_fall).then((user) => {
+        // console.log(user)
+    res.status(201).send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+
+app.post("/smartcontainer/insert_fall", (req, res) => {
+
+    req.body.creation_date=new Date().toISOString() 
+    // console.log(req.body.creation_date)
+    const user = new User(req.body)
+    // console.log(user)
+
+
+    // console.log(user)
+    user.save().then(() => {
+    res.send(user)
+    }).catch((e) => {
+    res.status(400).send(e)
+})
+})
+
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
